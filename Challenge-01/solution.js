8f9af1bf6779df5e43721d0ea62511ffd6d4fbff
Upendhar Nemmani <80301044+Upendhar10@users.noreply.github.com>
2024-02-08 09:06:50 +0530

    Create solution.js

---
+++ Challenge-01/solution.js
@@ -0,0 +1,17 @@
+let from = 10;
+let Till = 20;
+
+console.log(" Even numbers : ");
+for(let i = from; i < Till; i++){
+   if(i % 2 == 0){
+      console.log(i);
+    }
+}    
+console.log();
+console.log(" Odd numbers : ");
+let j = from;
+while( j < Till){
+  if(j % 2 != 0){
+      console.log(j);
+  }
+}
